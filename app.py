c220bf8297e2979eb3437d3306cdf40ca6adccc6
MuhammadUsmanAnees <muhammadusmananees66@gmail.com>
2026-01-11 13:00:29 +0500

    The web app is added

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ Scaler = pickle.load(open("Scaling.pkl", "rb"))
 def home():
     return render_template('home.html')
 
+# we created a Scaling.pkl because we want to first standardise the entire data then we will do the prediction
 @app.route('/predict_api', methods=['POST'])          # This creates an endpoint called /predict   # So we have created a predcit api 
 def predict_api():
     data = request.json['data']            # This reads the data you send from Postman in JSON format.
@@ -21,7 +22,16 @@ def predict_api():
     output = regmodel.predict(new_data)
     print(output[0])   # As it would be in two dimensional array so take the first value
     return jsonify(output[0])
-# we created a Scaling.pkl because we want to first standardise the entire data then we will do the prediction
+
+# Now create a web application with a form where we will add values and predict the results 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input=Scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The House Price prediction is {}".format(output))
+
 
 if __name__=="__main__":
     app.run(debug=True)
